87053109bf47861d7dc6e8f9fbcfd17cef303f87
Kristina Tong <kristina.tong@gmail.com>
2018-08-03 00:15:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find{|x|
-  if cheese_types.include?(x)
-    x
-  end
+  cheese_types.include?(x)
+  x
   }
 end
